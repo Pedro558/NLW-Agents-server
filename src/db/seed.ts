07becb9fd66309de125955e45c86743ec63dccab
pedro558 <pedroafonso109@gmail.com>
2025-07-16 12:21:19 -0300

    :sparkles: feat: creating 'questions' table

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,9 +1,9 @@
-import { reset, seed } from 'drizzle-seed'
-import { db, sql } from './connection.ts'
+import { reset, seed } from "drizzle-seed";
+import { db, sql } from "./connection.ts";
 
-import { schema } from './schema/index.ts'
+import { schema } from "./schema/index.ts";
 
-await reset(db, schema)
+await reset(db, schema);
 
 await seed(db, schema).refine((f) => {
   return {
@@ -11,12 +11,15 @@ await seed(db, schema).refine((f) => {
       count: 20,
       columns: {
         name: f.companyName(),
-        description: f.loremIpsum()
+        description: f.loremIpsum(),
+      },
+      questions:{
+        count: 20
       }
-    }
-  }
-})
+    },
+  };
+});
 
-await sql.end()
+await sql.end();
 
-console.log('Database seeded successfully!')
+console.log("Database seeded successfully!");
